43f37ff712fb38179c8643ceb92fda2636415cc5
Quint <me@di-wu.be>
2020-09-26 11:40:39 +0200

    Add npm to node install script.

--- .scripts/install-node.sh
+++ .scripts/install-node.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get install software-properties-common
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash –
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash –
 
-sudo apt-get install nodejs
+sudo apt-get install nodejs npm
 node -v && npm -v
 
 npm config set prefix ~/.local
+npm install -g cowsay
+cowsay nodejs and npm are installed
