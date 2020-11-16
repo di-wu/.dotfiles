e9f076e7e599ad3aff432e12713f27d2d8640e10
Quint <me@di-wu.be>
2020-11-16 09:32:49 +0100

    Use n package when installing node.

--- .scripts/install-node.sh
+++ .scripts/install-node.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install software-properties-common
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash –
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 
-sudo apt-get install nodejs npm
+sudo apt-get install -y nodejs npm
+
+echo "Cleaning npm cache:"
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n latest
+
 node -v && npm -v
 
-npm config set prefix ~/.local
-npm install -g cowsay
-cowsay nodejs and npm are installed
+
