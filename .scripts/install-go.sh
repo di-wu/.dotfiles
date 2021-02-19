7c6bae99eba3bbad3814b6d51f73b64755d0987e
Quint <me@di-wu.be>
2021-02-19 14:20:29 +0100

    Update Go(lang) version to 1.16.

--- .scripts/install-go.sh
+++ .scripts/install-go.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O go1.15.5.tar.gz
-tar -C /usr/local -xzf go1.15.5.tar.gz
+wget https://golang.org/dl/go1.16.linux-amd64.tar.gz -O go1.16.tar.gz
+tar -C /usr/local -xzf go1.16.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-rm go1.15.5.tar.gz
+rm go1.16.tar.gz
 go version
+
