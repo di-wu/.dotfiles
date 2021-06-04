5a71902b32074c9b21ce971c907a4231e541446e
Quint Daenen <me@di-wu.be>
2021-06-04 17:56:33 +0200

    Add some more pkgs.

---
+++ .scripts/install/apt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install \
+  git \
+  vim \
+  curl \
+  ffmpeg
+
--- .scripts/install/snap.sh
+++ .scripts/install/snap.sh
@@ -3,21 +3,19 @@
 # Update all installed snaps.
 sudo snap refresh
 
-# Install Discord if not installed already.
-if [[ $(snap list | grep discord) == 0 ]]; then
-  read -p "Do you want to install Discord? " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sudo snap install discord
+pkgList=(
+  "discord" 
+  "slack --classic" 
+  "spotify"
+)
+ 
+for pkg in "${pkgList[@]}"; do
+  if [[ -z $(snap list | grep ${pkg%% *}) ]]; then
+    read -p "Do you want to install ${pkg}? " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo snap install $pkg
+    fi
   fi
-fi
-
-# Install Slack if not installed already.
-if [[ $(snap list | grep slack) == 0 ]]; then
-  read -p "Do you want to install Slack? " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sudo snap install slack --classic
-  fi
-fi
+done
 
